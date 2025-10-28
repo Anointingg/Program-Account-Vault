d9687629a9c950d48749fa342d97c156582c44be
yorubad-dev <adetoyeanointing15@gmail.com>
2025-10-28 18:26:24 +0100

    create program derived account vault data structure and Vault structure to use in the program

--- programs/program_account_vault/src/lib.rs
+++ programs/program_account_vault/src/lib.rs
@@ -14,3 +14,25 @@ pub mod program_account_vault {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Vault {
+    owner: Pubkey,
+    balance: u64,
+    bump: u64,
+}
+
+#[derive(Accounts)]
+pub struct VaultAction<'info> {
+    #[account(mut)]
+    signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 6,
+        seeds = [b"vault", signer.key().as_ref()],
+        bump,
+    )]
+    vault: Account<'info, Vault>,
+    system_program: Program<'info, System>,
+}
